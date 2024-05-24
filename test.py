24706b76d7d6af88f516a224ea4f4374a8f29f91
bryt-frost <100052863+bryt-frost@users.noreply.github.com>
2024-05-24 17:31:50 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,1 @@
-import requests
-from meta_ai_api import MetaAI
 
-ai = MetaAI()
-response = ai.prompt(message="who is Osama bin ladin")
-
-if response:
-
-    print("============== OUTPUT START =============")
-    print(response)
-    print("============== OUTPUT END ===============")
-else:
-    print("============== OUTPUT START =============")
-    print("something")
-    print("============== OUTPUT END ===============")
